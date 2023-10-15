28b61eb405925d8dd4b3dc48a1d16e58bf1e4ebc
mkrzyzyn <maciej.krzyzynski@gmail.com>
2023-10-15 11:11:29 +0200

    Loan offers table.

--- src/app/services/backend.service.ts
+++ src/app/services/backend.service.ts
@@ -8,25 +8,18 @@ import { Observable } from 'rxjs';
 export class BackendService {
 
   private username = 'user';
-  private password = '60015829-2f2f-4c60-9c13-d69457a8b539';
+  private password = '917ca492-e8d8-4348-8d9d-72378b8a0377';
   
   private credentials = btoa(`${this.username}:${this.password}`);
 
   private baseUrl = 'http://localhost:8080';
 
-  private headers = this.createHeaders();
-
   private http_headers = new HttpHeaders({
     'Authorization': `Basic ${this.credentials}`,
-    'Content-Type': 'application/json'
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
   });
 
-  private createHeaders(): HeadersInit {
-    const headers = new Headers();
-    headers.append('Origin', 'http://localhost:8000');
-    headers.append('Authorization', `Basic ${this.credentials}`);
-    return headers;
-  }
 
   constructor(private http: HttpClient) {}
 
@@ -34,30 +27,6 @@ export class BackendService {
     return this.http.get(`${this.baseUrl}/api/offers`, {headers: this.http_headers});
   }
 
-  private apiUrl = 'http://localhost:8080/api/offers';
-
-  fetchData() {
-    return fetch(this.apiUrl, {
-      method: 'GET',
-      headers: this.headers
-      // headers: {
-      //   'Authorization': `Basic ${this.credentials}`
-      // },
-      // mode: 'no-cors'
-    })
-      .then(response => {
-        if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        return response.json();
-      })
-      .then(data => {
-        return data;
-      })
-      .catch(error => {
-        throw error;
-      });
   }
 
   // Add methods for other CRUD operations (POST, PUT, DELETE) as needed.
-}
